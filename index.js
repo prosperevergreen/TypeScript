3eaee2ce0c03c85ce9328071e9c32f1a6e9b9b63
prosperevergreen <prosperevergreen@gmail.com>
2020-07-07 01:08:17 +0300

    created base

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var Greeting = /** @class */ (function () {
+    function Greeting() {
+    }
+    Greeting.prototype.greet = function () {
+        console.log("Hello world!!!");
+    };
+    return Greeting;
+}());
+var obj = new Greeting();
+obj.greet();
+var water = "food";
+water = "5";
+var a;
+var b;
---
+++ index.ts
@@ -0,0 +1,17 @@
+export {};
+
+class Greeting {
+  greet(): void {
+    console.log("Hello world!!!");
+  }
+}
+
+var obj = new Greeting();
+obj.greet();
+
+var water: string = "food";
+
+water = "5";
+var a;
+var b;
+
